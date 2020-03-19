254fd03eeb37dbdd367b205debdce7c254c40b4a
Chris Moreton <chris.moreton@hmcts.net>
2020-03-19 15:37:04 +0000

    Add legality test

--- src/test/java/com/netsensia/rivalchess/openings/OpeningLibraryTest.java
+++ src/test/java/com/netsensia/rivalchess/openings/OpeningLibraryTest.java
@@ -1,8 +1,11 @@
 package com.netsensia.rivalchess.openings;
 
+import com.netsensia.rivalchess.model.Board;
+import com.netsensia.rivalchess.model.Move;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertTrue;
 
@@ -14,6 +17,15 @@ public class OpeningLibraryTest {
             assertTrue(Arrays.asList("b8c6", "g7g6", "a7a6")
                     .contains(OpeningLibrary.getMove(
                             OpeningLibrary.E2E4_C7C5_G1F3_D7D6_D2D4_C5D4_F3D4_G8F6_B1C3).toString()));
+        }
+    }
+
+    @Test
+    public void testAllMovesAreLegal() {
+        for (OpeningPosition opening : OpeningLibrary.getOpenings()) {
+            final Board board = Board.fromFen(opening.getFen());
+            final List<Move> legalMoves = board.getLegalMoves();
+            assertTrue(legalMoves.contains(opening.getMove()));
         }
     }
 }
